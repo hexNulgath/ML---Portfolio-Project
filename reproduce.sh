b2d5fc65055b2d72928dd29314a6aed437a63df7
hexNulgath <machadoignacio@hotmail.com>
2026-01-20 16:53:10 -0300

    Update MOVEIT notebook and reproduce script
    - Added TEST_START and TEST_END variables for simulation time control in MOVEIT.ipynb.
    - Updated SUMO_CMD to include new parameters for simulation execution.
    - Modified execution counts for various code cells in MOVEIT.ipynb.
    - Improved reward calculation and logging in the notebook.
    - Changed the execution command in reproduce.sh to ensure in-place execution of the notebook.
    - Added Miniconda installer to .gitignore to prevent tracking of installation files.

--- reproduce.sh
+++ reproduce.sh
@@ -4,6 +4,6 @@ echo "1. Installing requirements..."
 pip install -r requirements.txt
 
 echo "2. Running analysis..."
-jupyter nbconvert --execute notebooks/MOVEIT.ipynb
+jupyter nbconvert --to notebook --execute --inplace notebooks/MOVEIT.ipynb
 
 echo "3. Outputs available in results/"
